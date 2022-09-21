ae7d7c4a78482e3758e3c2bb650e2e01dacaf7ba
diegodmca <diegodmca@outlook.com>
2022-09-21 10:16:47 -0300

    implementacao carrinho/contato/barra de pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduto, produtos } from '../produtos';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from '../produtos';
+import { ProdutosService } from '../produtos.service';
 
 @Component({
   selector: 'app-produtos',
@@ -8,11 +10,23 @@ import { IProduto, produtos } from '../produtos';
 })
 export class ProdutosComponent implements OnInit {
 
-  produtos: IProduto[] = produtos;
+  produtos: IProduto[] | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private produtosService: ProdutosService,
+    private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    const produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+      
+      if (descricao) {
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return;
+      }
+      this.produtos = produtos;
+
+    })
+  }
 }
